d32c754fe4847c8cf07c1842777ead7c59174b1f
Ishan Deep <ishandeep48@gmail.com>
2025-09-05 18:05:55 +0530

    Added Google Maps

--- CrowdSource/src/components/User.jsx
+++ CrowdSource/src/components/User.jsx
@@ -1,6 +1,8 @@
 import { useState, useRef } from "react";
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 
 export default function User() {
+  const API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
   let priorityOptions = ["low", "medium", "high", "critical"];
   const formDataStruct = {
     issue: "",
@@ -8,30 +10,46 @@ export default function User() {
     picture: "",
     location: {
       lat: 0,
-      long: 0,
+      lng: 0,
     },
   };
   const [formData, setFormData] = useState(formDataStruct);
-  const [error,setError] = useState(null);
-  const getLocation =()=>{
-    if(!navigator.geolocation){
-        setError("Geolocation is not supported by your browser");
-        return;
+  const [error, setError] = useState(null);
+  const getLocation = () => {
+    if (!navigator.geolocation) {
+      setError("Geolocation is not supported by your browser");
+      return;
     }
-    navigator.geolocation.getCurrentPosition((pos)=>{
+    navigator.geolocation.getCurrentPosition(
+      (pos) => {
         setFormData({
-            ...formData,
-            location:{
-                lat:pos.coords.latitude,
-                long:pos.coords.longitude
-            }
+          ...formData,
+          location: {
+            lat: pos.coords.latitude,
+            lng: pos.coords.longitude,
+          },
         });
-    },(err)=>{
+      },
+      (err) => {
         setError(err.message);
-    }
-    )
+      }
+    );
+  };
+  const containerStyle = {
+    width: "500px",
+    height: "500px",
+  };
+  const mapClickHandler =(e) =>{
+    const lat = e.latLng.lat();
+    const lng = e.latLng.lng();
+    setFormData({
+      ...formData,
+      location: {
+        lat: lat,
+        lng: lng,
+      },
+    });
   }
-
   return (
     <>
       <input
@@ -51,11 +69,22 @@ export default function User() {
         })}
       </select>
       <div>
-        <p>Your Longitude is: {formData.location.long}</p>
+        <p>Your Longitude is: {formData.location.lng}</p>
         <p>Your Latitude is: {formData.location.lat}</p>
       </div>
       <button onClick={getLocation}>Click to get Your Location</button>
       <button onClick={() => console.log(formData)}>Submit</button>
+      <LoadScript googleMapsApiKey={API_KEY}>
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={formData.location}
+          zoom={10}
+          onClick={mapClickHandler}
+        >
+          <Marker position={formData.location} />
+        </GoogleMap>
+
+      </LoadScript>
     </>
   );
 }
